bcdb53d1566a81ab5b648a44c5b46de52a3e85f7
mmavrich <mmavrich@gmail.com>
2020-11-12 10:27:11 -0500

    Fixed print statement

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -9,21 +9,23 @@ from datetime import date, datetime
 data = []
 print("Sensor Data App")
 ##############################
+
+# Module 1 code here:
 data=load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 
-# Module 1 code here:
+# Module 2 code here:
 house_info=HouseInfo(data)
 test_area=1
 
-recs=house_info.get_data_by_area("id",test_area)
+recs=house_info.get_data_by_area("id",rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 test_date = datetime.strptime("5/9/20","%m/%d/%y")
-recs=house_info.get_data_by_date("id",test_date)
+recs=house_info.get_data_by_date("id",rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
-# Module 2 code here:
+
 
 # Module 3 code here:
 
